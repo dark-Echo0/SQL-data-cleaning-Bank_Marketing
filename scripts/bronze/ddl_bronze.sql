c2bd12016da10f958f75f4d5616184122620d838
Ric Araojo <ojoararico.me@gmail.com>
2025-09-08 10:55:09 +0800

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,10 @@
+/*
+=================================================================================
+DDL Script: Create Bronze Tables
+=================================================================================
+Script Purpose:
+    This script creates tables in the 'bronce' schema, dropping existing tables
+    if they already exist.
+    Run this script to re-define the DDL structure of 'bronze' Tables
+=================================================================================
+*/
